79a06fc1a70a5bdce399c4022e9d31aa0c0450a8
lei.yao <yoing945@outlook.com>
2019-08-15 15:27:14 +0800

    添加初始脚本

---
+++ Project/Assets/Scripts/Config/ConfigsLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigsLoader : MonoBehaviour
+{
+    //使用Resource Load方式
+    private const string CONFIG_ROOT_PATH = "";
+    public const string COLOR_DEFINE_CONFIG_PATH = 
+        "";
+}
